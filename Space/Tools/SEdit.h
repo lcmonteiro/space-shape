40923c04894eb91178416a7b6a7dcd8652bf6063
Monteiro <monteiro.lcm@gmail.com>
2019-05-09 23:56:45 +0100

    review: start

--- Space/Tools/SEdit.h
+++ Space/Tools/SEdit.h
@@ -1,103 +1,101 @@
-/*
+/**
+ * ------------------------------------------------------------------------------------------------
  * File:   SEdit.h
  * Author: Luis Monteiro
  *
  * Created on Apr 10, 2019, 12:11 PM
+ * ------------------------------------------------------------------------------------------------
  */
-
 #ifndef SEDIT_H
 #define SEDIT_H
 /**
+ * space
  */
 #include "SVariable.h"
 /**
  */
-using namespace std;
-/**
- */
-typedef class Sedit {
-public:
-        /*-------------------------------------------------------------------------------------------------------------*
+namespace Edit {
+        /**
+         * --------------------------------------------------------------------
          *                                   ___ _____ ___       _________ ___
          * Insert var (A) on other var(B)   |_A_|_A&B_|_B_| ->  |____A____|_B_| 
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Var Insert(Var var, Var on);
-        //
-        static Map& Insert(const Map& var, Map& on);
-        //
-        static List& Insert(const List& var, List& on);
+         * ---------------------------------------------------------------------
+         */
+        Var&  Insert(const Var& var,  Var& on);
+        Map&  Insert(const Map& var,  Map& on);
+        List& Insert(const List& var, List& on);
         /**
          * Insert utilities 
          */
-        static inline Var Insert(List&& var, Var on = nullptr) {
-                return Insert(var, on);
+        Var Insert(List&& var, Var on = nullptr) {
+            return Insert(var, on);
         }
-        static inline Var Insert(List& var, Var on = nullptr) {
+        Var Insert(List& var, Var on = nullptr) {
                 for (Var v : var) {
                         on = Insert(v, on);
                 }
                 return on;
         }
-        /*-------------------------------------------------------------------------------------------------------------* 
+        /**
+         * -------------------------------------------------------------------- 
          *                                   ___ _____ ___       _ _____ ___ 
          * Update var (A) on other var (B)  |_A_|_A&B_|_B_| ->  ~_|__A__|_B_| 
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Var Update(Var var, Var on);
-        //
-        static Map& Update(const Map& var, Map& on);
-        //
-        static List& Update(const List& var, List& on);
+         * --------------------------------------------------------------------
+         */
+        Var&  Update(const Var&  var, Var   on);
+        Map&  Update(const Map&  var, Map&  on);
+        List& Update(const List& var, List& on);
         /**
          * Update utilities 
          */
-        static inline Var Update(List&& var, Var on = nullptr) {
+        Var Update(List&& var, Var on = nullptr) {
                 return Update(var, on);
         }
-        static inline Var Update(List& var, Var on = nullptr) {
+        Var Update(List& var, Var on = nullptr) {
                 for (Var v : var) {
                         on = Update(v, on);
                 }
                 return on;
         }
-        /*-------------------------------------------------------------------------------------------------------------*
+        /**
+         * --------------------------------------------------------------------
          *                                   ___ _____ ___       _ _____ _
          *  Find var (A) on other var (B)   |_A_|_A&B_|_B_| ->  ~_|_A&B_|_~
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Var Find(Var var, Var on);
-        //
-        static Map& Find(Map& var, const Map& on);
-        //
-        static List& Find(List& var, const List& on);
-        /*-------------------------------------------------------------------------------------------------------------*
+         * --------------------------------------------------------------------
+         */
+        Var&  Find(Var& var,  const Var&  on);
+        Map&  Find(Map& var,  const Map&  on);
+        List& Find(List& var, const List& on);
+        /**
+         * --------------------------------------------------------------------
          *                                   ___ _____ ___       _ ___
          *  Remove var (A) on other var (B) |_A_|_A&B_|_B_| ->  ~_|_B_|
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Var Remove(Var var,  Var on);
-        //
-        static Map& Remove(const Map& var, Map& on);
-        //
-        static List& Remove(const List& var, List& on);
+         * --------------------------------------------------------------------
+         */
+        Var   Remove(const Var& var,  Var   on);
+        Map&  Remove(const Map& var,  Map&  on);
+        List& Remove(const List& var, List& on);
         /**
          * Remove utilities 
          */
-        static inline Var Remove(List&& var, Var on = nullptr) {
+        Var Remove(List&& var, Var on = nullptr) {
                 return Remove(var, on);
         }
-        static inline Var Remove(List& var, Var on = nullptr) {
+        Var Remove(List& var, Var on = nullptr) {
                 for (Var v : var) {
                         on = Remove(v, on);
                 }
                 return on;
         }
-        /*-------------------------------------------------------------------------------------------------------------*
+        /**
+         * -------------------------------------------------------------------
          *                                                            ___ _____ ___       _ ___
          *  Delete var (A) on other var (B) and clear empty branches |_A_|_A&B_|_B_| ->  ~_|_B_|
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Var Delete(Var var, Var on);
-        //
-        static Map& Delete(const Map& var, Map& on);
-        //
-        static List& Delete(const List& var, List& on);
+         * --------------------------------------------------------------------
+         */
+        Var&  Delete(const Var& var,  Var&  on);
+        Map&  Delete(const Map& var,  Map&  on);
+        List& Delete(const List& var, List& on);
         /**
          * Remove utilities
          */
@@ -110,80 +108,75 @@ public:
                 }
                 return on;
         }
-        /*-------------------------------------------------------------------------------------------------------------*
-         * 
+        /**
+         * --------------------------------------------------------------------
          * verify match from var (A) on (B)
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Boolean Match(Var var, Var on);
-        //
-        static Boolean Match(const Map& v, const Map& on);
-        //
-        static Boolean Match(const List& v, const List& on);
-        /*-------------------------------------------------------------------------------------------------------------*
-         * 
+         * --------------------------------------------------------------------
+         */
+        Boolean Match(const Var&  var, const Var&  on);
+        Boolean Match(const Map&  var, const Map&  on);
+        Boolean Match(const List& var, const List& on);
+        /**
+         * --------------------------------------------------------------------
          * Count var (A) on other var (B)
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Integer Count(String var, Var on);
-        //
-        static inline Integer Count(Var var, Var on) {
-                return Count(String(var), on);
-        };
-        /*-------------------------------------------------------------------------------------------------------------*
-         * 
+         * --------------------------------------------------------------------
+         */
+        Integer Count(String var, Var on);
+        /**
+         * --------------------------------------------------------------------
          * Insert Key:var on other var 
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Var Insert(Key path, Var var, Var on = nullptr);
-        //
-        static inline Var Insert(Map&& var, Var on = nullptr) {
-                return Insert(var, on);
-        }
-        //
-        static inline Var Insert(Map& var, Var on = nullptr) {
+         * --------------------------------------------------------------------
+         */
+        Var Insert(Key path, Var var, Var on = nullptr);
+        /**
+         * derive
+         */
+        inline Var Insert(Map& var, Var on = nullptr) {
                 for (auto& v : var) {
                         on = Insert(v.first, v.second, on);
                 }
                 return on;
         }
-        //
-        static inline List Insert(Map&& var, List on) {
-                return move(Insert(var, on));
+        inline Var Insert(Map&& var, Var on = nullptr) {
+                return Insert(var, on);
         }
-        //
-        static inline List Insert(Map& var, List on) {
+        inline List Insert(Map& var, List on) {
                 for (auto& e : on) { 
                         Insert(var, e);
                 }
                 return move(on);
         }
-        /*-------------------------------------------------------------------------------------------------------------*
-         * 
+        inline List Insert(Map&& var, List on) {
+                return move(Insert(var, on));
+        }
+        /**
+         * --------------------------------------------------------------------
          * Update Key:var on other var 
-         *-------------------------------------------------------------------------------------------------------------*/
-        static Var Update(Key path, Var var, Var on = nullptr);
-        //
-        static inline Var Update(Map&& var, Var on = nullptr) {
+         * --------------------------------------------------------------------
+         */
+        Var Update(Key path, Var var, Var on = nullptr);
+        /**
+         * derived
+         */
+        inline Var Update(Map&& var, Var on = nullptr) {
                 return Update(var, on);
         }
-        //
-        static inline Var Update(Map& var, Var on = nullptr) {
+        inline Var Update(Map& var, Var on = nullptr) {
                 for (auto& v : var) {
                         on = Update(v.first, v.second, on);
                 }
                 return on;
         }
-        //
-        static inline List Update(Key path, Var var, List on) {
+        inline List Update(Key path, Var var, List on) {
                 for (Var v : on) {
                         Update(path, var, v);
                 }
                 return move(on);
         }
-        //
-        static inline List Update(Map&& var, List on) {
+        inline List Update(Map&& var, List on) {
                 return move(Update(var, on));
         }
-        //
-        static inline List Update(Map& var, List on) {
+        inline List Update(Map& var, List on) {
                 for (auto& e : on) { 
                         Update(var, e);
                 }
@@ -192,7 +185,7 @@ public:
         /**
          * Update all branches deeper than (deep) with (var)
          */
-        static Var Update(Integer deep, Var var, Var on);
+        Var Update(Integer deep, Var var, Var on);
         
         /*-------------------------------------------------------------------------------------------------------------*
          * 
@@ -279,7 +272,7 @@ public:
         }
         //
         static inline Var Move(Var from, Var map, Var to = Obj::Map()) {
-                return Move(from, toMapKey(map), to);
+                return Move(from, ToMapKey(map), to);
         }
         /*-------------------------------------------------------------------------------------------------------------*
          * Link
@@ -290,30 +283,35 @@ public:
                 }
                 return to;
         }
-        //
         static inline Var Link(Var from, MapKey&& map, Var to = Obj::Map()) {
                 return Link(from, map, to);
         }
-        //
         static inline Var Link(Var from, Var map, Var to = Obj::Map()) {
-                return Link(from, toMapKey(map), to);
+                return Link(from, ToMapKey(map), to);
         }
-        /*-------------------------------------------------------------------------------------------------------------*
+        /**
+         * --------------------------------------------------------------------
          * Normalize var ()
-         *-------------------------------------------------------------------------------------------------------------*/
+         * --------------------------------------------------------------------
+         */ 
         static Var Normalize(Var var);
-        /*-------------------------------------------------------------------------------------------------------------*
+        /**
+         * --------------------------------------------------------------------
          * Utilities
-         *-------------------------------------------------------------------------------------------------------------*/
-        static inline MapKey toMapKey(Var map){
-                MapKey ret;
-                for (auto& v : Var::ToMap(map)) {
-                        ret[v.first] = Var::ToString(v.second);
-                }
-                return ret;
+         * --------------------------------------------------------------------
+         */
+        static inline MapKey ToMapKey(Var map){
+        MapKey ret;
+        for (auto& v : Var::ToMap(map)) {
+                ret[v.first] = Var::ToString(v.second);
         }
-} Edit;
-
-
-#endif        /* SEDIT_H */
+        return ret;
+        }
+}
+/**
+ * ------------------------------------------------------------------------------------------------
+ * End
+ * ------------------------------------------------------------------------------------------------
+ */
+#endif /* SEDIT_H */
 
