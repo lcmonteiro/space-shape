f91150e9bc9466abf4e34c6ed926f37caa60f0b4
Monteiro <monteiro.lcm@gmail.com>
2019-05-29 01:16:00 +0100

    update : learner

--- Applications/MergeXML/Commands/SLearn.cpp
+++ Applications/MergeXML/Commands/SLearn.cpp
@@ -13,6 +13,7 @@
  * space 
  */
 #include "SLog.h"
+#include "SLogic.h"
 #include "SLearn.h"
 #include "SFileSystem.h"
 /**
@@ -26,9 +27,25 @@
  * ----------------------------------------------------------------------------
  */
 Var Command::Explorer(const String& path, String pattern) { 
-    return FileSystem::Find(path, [&pattern](auto path, int type) {
-        return std::regex_match(path.back(), std::regex(pattern));
+    Var var = Obj(nullptr);
+    /**
+     * read from file system
+     */
+    var = FileSystem::Find(path, [&pattern](auto path, auto type) {
+        return (FileSystem::DIR == type) 
+            || std::regex_match(path.back(), std::regex(pattern));
     });
+    /**
+     * filter empty folders
+     */ 
+    var = Logic::ForEach(var, [](auto, auto var) {
+        return Obj(3);
+        //return (Var::ToInteger(var) == FileSystem::DIR)? Obj(nullptr) : var;  
+    });
+    /**
+     * return 
+     */
+    return var;
 }  
 /**
  * ----------------------------------------------------------------------------
