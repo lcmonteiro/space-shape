6c876328f0a5209c8ed2eb9abad1fe0a8e940290
Monteiro <monteiro.lcm@gmail.com>
2019-06-02 22:40:39 +0100

    update : learner :: explorer

--- Applications/MergeXML/Commands/SLearn.cpp
+++ Applications/MergeXML/Commands/SLearn.cpp
@@ -15,6 +15,8 @@
 #include "SLog.h"
 #include "SLogic.h"
 #include "SLearn.h"
+#include "SUtils.h"
+#include "SConvert.h"
 #include "SFileSystem.h"
 /**
  * ----------------------------------------------------------------------------
@@ -38,16 +40,20 @@ Var Command::Explorer(const String& path, String pattern) {
     /**
      * filter empty folders
      */ 
-    var = Logic::ForEach(var, [](auto, auto var) {
+    var = Var::Trim(Logic::ForEach(var, [](auto, auto var) {
         if(Var::IsInteger(var)) {
             return (Var::Integer(var) == FileSystem::DIR)? Obj(nullptr) : var;  
         }
         return var;
-    });
+    }));
     /**
-     * return 
+     * return a list of path files
      */
-    return var;
+    return Obj(Logic::ForEach(
+        Utils::GetKeys(Convert::ToSimpleMap(var, "/")), [&path](auto, auto var){
+            return Obj(String::Build(path, "/", Var::String(var)));
+        }
+    ));
 }  
 /**
  * ----------------------------------------------------------------------------
